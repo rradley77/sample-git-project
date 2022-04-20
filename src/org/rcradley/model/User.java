c3741de16d6c9fe3baa5f79d0069eb157012d67d
rradley77 <rcr77@me.com>
2022-04-20 17:17:23 -0400

    recreated toString method

--- src/org/rcradley/model/User.java
+++ src/org/rcradley/model/User.java
@@ -22,13 +22,15 @@ public class User {
 		this.age = age;
 	}
 
-	@Override
-	public String toString() {
-		return "User [name=" + name + ", age=" + age + "]";
-	}
+
 
 	
 	
+
+	@Override
+	public String toString() {
+		return "User [name=" + name + ", password=" + password + ", age=" + age + "]";
+	}
 
 	public String getPassword() {
 		return password;

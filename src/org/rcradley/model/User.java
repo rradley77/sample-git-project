09d1fe456127297a27e6451f2c1a847170e19979
rradley77 <rcr77@me.com>
2022-04-20 16:42:02 -0400

    added nanme getters and setters

--- src/org/rcradley/model/User.java
+++ src/org/rcradley/model/User.java
@@ -2,5 +2,13 @@ package org.rcradley.model;
 
 public class User {
 	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 	
 }
